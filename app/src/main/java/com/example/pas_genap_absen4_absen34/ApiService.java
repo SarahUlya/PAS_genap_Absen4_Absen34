b01273f2379c3cea48daed3d3ebec4b08d475a1f
KeikoW0 <keikoshafirawiyana@gmail.com>
2025-05-26 10:29:52 +0700

    feat [KW] new api spanish

--- app/src/main/java/com/example/pas_genap_absen4_absen34/ApiService.java
+++ app/src/main/java/com/example/pas_genap_absen4_absen34/ApiService.java
@@ -2,10 +2,15 @@ package com.example.pas_genap_absen4_absen34;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface ApiService {
     @GET("all_countries.php")
     Call<CountryResponse> getAll_countries();
 
+    @GET("search_all_teams.php")
+    Call<SpanishTeamResponse> getTeams(
+            @Query("s") String sport,
+            @Query("c") String country
+    );
 }
-
